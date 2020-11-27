22118cbe041c4791d884f4bcc9586fd2ab38c2af
Leonardo Teixeira <leovenancio00@gmail.com>
2020-11-27 18:06:42 -0300

    CheckBoxList tile implementado

--- lib/main.dart
+++ lib/main.dart
@@ -26,9 +26,9 @@ class HomePage extends StatefulWidget {
 
   HomePage() {
     items = [];
-    items.add(Item(title: "Item 1", done: false));
-    items.add(Item(title: "Item 2", done: true));
-    items.add(Item(title: "Item 3", done: false));
+    items.add(Item(title: "Banana 1", done: false));
+    items.add(Item(title: "Limao 2", done: true));
+    items.add(Item(title: "Jaca 3", done: false));
   }
 
   @override
@@ -45,7 +45,15 @@ class _HomePageState extends State<HomePage> {
       body: ListView.builder(
         itemCount: widget.items.length,
         itemBuilder: (BuildContext ctxt, int index) {
-          return Text(widget.items[index].title);
+          final item = widget.items[index];
+          return CheckboxListTile(
+            title: Text(item.title),
+            key: Key(item.title),
+            value: item.done,
+            onChanged: (value) {
+              print(value);
+            },
+          );
         },
       ),
     );
