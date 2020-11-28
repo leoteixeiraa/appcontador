82a02a7153d1449ab9c56b69ca4269b9904510d7
Leonardo Teixeira <leovenancio00@gmail.com>
2020-11-27 23:05:22 -0300

    Salvando Itens

--- lib/main.dart
+++ lib/main.dart
@@ -51,12 +51,14 @@ class _HomePageState extends State<HomePage> {
         ),
       );
       newTaskCtrl.text = "";
+      save();
     });
   }
 
   void remove(int index) {
     setState(() {
       widget.items.removeAt(index);
+      save();
     });
   }
 
@@ -71,6 +73,11 @@ class _HomePageState extends State<HomePage> {
         widget.items = result;
       });
     }
+  }
+
+  save() async {
+    var prefes = await SharedPreferences.getInstance();
+    await prefes.setString('data', jsonEncode(widget.items));
   }
 
   _HomePageState() {
@@ -105,6 +112,7 @@ class _HomePageState extends State<HomePage> {
               onChanged: (value) {
                 setState(() {
                   item.done = value;
+                  save();
                 });
               },
             ),
